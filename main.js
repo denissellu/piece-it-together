d09d8f36b325e3a72fc2b2aed40dcdf7d086cbc7
Denis Sellu <denis.sellu@ably.com>
2023-06-06 14:48:57 +0300

    🧩 Add images to puzzle pieces

--- main.js
+++ main.js
@@ -2,33 +2,40 @@ import './style.css'
 
 const headbreaker = window.headbreaker
 
-const jigsaw = new headbreaker.Canvas('puzzle', {
-  width: 1000,
-  height: 1000,
-  preventOffstageDrag: true,
-  pieceSize: 100,
-  outline: new headbreaker.outline.Rounded(),
-  proximity: 20,
-  borderFill: 10,
-  strokeWidth: 2,
-  lineSoftness: 0.18,
-  fixed: true
-})
+const puzzleImage = new Image()
 
-jigsaw.autogenerate({
-  horizontalPiecesCount: 3,
-  verticalPiecesCount: 3
-})
+puzzleImage.src = 'public/jigsaw-images/1.jpg'
 
-jigsaw.attachSolvedValidator()
+puzzleImage.onload = () => {
+  const jigsaw = new headbreaker.Canvas('puzzle', {
+    width: 1000,
+    height: 1000,
+    preventOffstageDrag: true,
+    pieceSize: 100,
+    outline: new headbreaker.outline.Rounded(),
+    proximity: 20,
+    borderFill: 10,
+    strokeWidth: 2,
+    lineSoftness: 0.18,
+    fixed: true,
+    image: puzzleImage
+  })
 
-jigsaw.onValid(() => {
-  setTimeout(() => {
-    if (jigsaw.puzzle.isValid()) {
-      alert('WHOOOOOP!!!!')
-    }
-  }, 500)
-})
+  jigsaw.autogenerate({
+    horizontalPiecesCount: 3,
+    verticalPiecesCount: 3
+  })
 
-jigsaw.shuffle(0.8)
-jigsaw.draw()
+  jigsaw.attachSolvedValidator()
+
+  jigsaw.onValid(() => {
+    setTimeout(() => {
+      if (jigsaw.puzzle.isValid()) {
+        alert('WHOOOOOP!!!!')
+      }
+    }, 500)
+  })
+
+  jigsaw.shuffle(0.8)
+  jigsaw.draw()
+}
