8f0c03abe92f6c81a6c4a43b86904738f30c9caa
Denis Sellu <denis.sellu@ably.com>
2023-06-07 02:04:13 +0300

    🧩 and the rest 🙈

--- main.js
+++ main.js
@@ -1,61 +1,198 @@
 import './style.css'
 import Spaces from '@ably-labs/spaces'
 import { nanoid } from 'nanoid'
-import { uniqueNamesGenerator, adjectives, animals } from 'unique-names-generator';
+import { uniqueNamesGenerator, adjectives, animals } from 'unique-names-generator'
+
+const cursorSvg = (startColor = '#06B6D4', endColor = '#ffffff', id) => {
+  return `
+    <svg width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0.391407 3.21084L7.76105 25.3198C8.27823 26.8713 10.2474 27.3361 11.4038 26.1797L26.1431 11.4404C27.2995 10.284 26.8347 8.31485 25.2831 7.79767L3.17421 0.42803C1.45434 -0.145257 -0.181883 1.49097 0.391407 3.21084Z" fill="url(#gradient-${id})"/>
+      <defs>
+        <linearGradient id="gradient-${id}" x1="28.6602" y1="-0.963373" x2="-0.999994" y2="28.6968" gradientUnits="userSpaceOnUse">
+          <stop stop-color="${startColor}"/>
+          <stop offset="1" stop-color="${endColor}"/>
+        </linearGradient>
+      </defs>
+    </svg>`
+}
 
 // Set up random name for game
 let gameName = uniqueNamesGenerator({
   dictionaries: [adjectives, animals],
   separator: '-',
-  length: 2,
+  length: 2
 })
+const clientId = nanoid()
 
-//See if you were invited in to a game
-const urlParams = new URLSearchParams(window.location.search);
-const gameNameFromParam = urlParams.get('game-id');
+// JOINING GAME
+// See if you were invited in to a game
+const urlParams = new URLSearchParams(window.location.search)
+const gameNameFromParam = urlParams.get('game-id')
 
-if(gameNameFromParam !== null) {
+if (gameNameFromParam !== null) {
   gameName = gameNameFromParam
 
-  startGame();
+  showSignInScreen()
 }
 
+// init and setup spaces
+const spaces = new Spaces({
+  key: 'awGFVA.GGryXw:IS_VqROlI1dRbD3mkM72hP8QPd4HiMAzzZTTAdtLVug',
+  clientId
+})
+const mySpace = spaces.get(gameName)
+
+// NEW GAME CREATION
 document.getElementById('create-new-game').addEventListener('click', function () {
-  const newUrl = new URL(window.location);
-  newUrl.searchParams.set('game-id', gameName);
+  const newUrl = new URL(window.location)
+  newUrl.searchParams.set('game-id', gameName)
 
-  window.history.pushState(null, '', newUrl.toString());
+  window.history.pushState(null, '', newUrl.toString())
 
-  startGame();
+  showSignInScreen()
 })
 
-function startGame() {
-  document.getElementById('welcome-screen').setAttribute('class', 'hide');
-  document.getElementById('game-screen').removeAttribute('class', 'hide');
+function signInPlayerInToSpace () {
+  const username = document.querySelector('#user-sign-in .username').value
+
+  mySpace.enter({
+    username
+  })
+}
+
+function showSignInScreen () {
+  document.getElementById('welcome-screen').setAttribute('class', 'hide')
+  document.getElementById('user-sign-in').removeAttribute('class', 'hide')
+}
+
+document.getElementById('join-game').addEventListener('click', function () {
+  signInPlayerInToSpace()
+  startGame()
+})
+
+// MAIN GAMEPLAY FUNCTION
+function startGame () {
+  document.getElementById('user-sign-in').setAttribute('class', 'hide')
+  document.getElementById('game-screen').removeAttribute('class', 'hide')
 
   // For the puzzle
   const headbreaker = window.headbreaker
 
   const puzzleImage = new Image()
-  const jsonExportArea = document.getElementById('json-export-area')
-
-  // init and setup spaces
-  const spaces = new Spaces({
-    key: '#',
-    clientId: nanoid()
-  })
-  const mySpace = spaces.get(gameName)
 
   document.getElementById('game-name').innerHTML += window.location.origin + '/?game-id=' + gameName
 
-  mySpace.on('membersUpdate', (members) => {
-    console.log(members)
-  })
-
-  mySpace.enter({ name: 'Denis' })
+  const playersConnected = (player) => player.isConnected === true
 
   puzzleImage.src = 'public/jigsaw-images/1.jpg'
   puzzleImage.onload = () => {
+    const updateGameChannel = mySpace.client.channels.get('updateGame')
+
+    mySpace.on('enter', (player) => {
+      const chosenPlayer = mySpace.getMembers().filter(playersConnected)
+
+      if (chosenPlayer.length !== 0) {
+        if (chosenPlayer[0].clientId === clientId) {
+          const gameState = jigsaw.puzzle.export({ compact: true })
+          updateGameChannel.publish('updateGameState', gameState)
+        }
+      }
+    })
+
+    // FOR THE CURSOR
+    const createCursor = (connectionId, profileData) => {
+      const container = document.createElement('div')
+      container.id = `cursor-${connectionId}`
+      container.classList.add('absolute')
+
+      const cursor = document.createElement('div')
+      cursor.innerHTML = cursorSvg(
+        '#' + Math.floor(Math.random() * 16777215).toString(16),
+        '#' + Math.floor(Math.random() * 16777215).toString(16),
+        connectionId
+      )
+
+      const label = document.createElement('p')
+      label.classList.add('absolute')
+
+      label.innerHTML = profileData.username
+
+      container.appendChild(cursor)
+      container.appendChild(label)
+
+      return container
+    }
+
+    const gameContainer = document.querySelector('#game-screen')
+    const cursor = mySpace.cursors.get(gameName)
+
+    cursor.on('cursorUpdate', update => {
+      if (update.clientId === clientId) {
+        return
+      }
+
+      let cursorNode = gameContainer.querySelector(
+        `#cursor-${update.connectionId}`
+      )
+
+      const playersInGame = mySpace.getMembers()
+
+      const player = playersInGame.find(
+        player => player.connectionId === update.connectionId
+      )
+
+      if (!player || !mySpace.getSelf().connectionId || self.connectionId === update.connectionId) {
+        return
+      }
+
+      if (!(cursorNode instanceof HTMLElement)) {
+        cursorNode = createCursor(update.connectionId, player.profileData)
+        gameContainer.appendChild(cursorNode)
+      }
+
+      if (update.data.state === 'leave') {
+        gameContainer.removeChild(cursorNode)
+      } else {
+        cursorNode.style.left = update.position.x + 'px'
+        cursorNode.style.top = update.position.y + 'px'
+      }
+    })
+
+    gameContainer.addEventListener('mouseenter', function () {
+      const { top, left } = gameContainer.getBoundingClientRect()
+
+      cursor.set({
+        position: { x: event.clientX - left, y: event.clientY - top },
+        data: { state: 'enter' }
+      })
+    })
+
+    const cursorSendPerSecond = 25
+    let cursorWaitUntil = 0
+
+    gameContainer.addEventListener('mousemove', function () {
+      if (Date.now() >= cursorWaitUntil) {
+        const { top, left } = gameContainer.getBoundingClientRect()
+
+        cursor.set({
+          position: { x: event.clientX - left, y: event.clientY - top },
+          data: { state: 'move' }
+        })
+
+        cursorWaitUntil = Date.now() + 1000 / cursorSendPerSecond
+      }
+    })
+
+    gameContainer.addEventListener('mouseleave', function () {
+      const { top, left } = gameContainer.getBoundingClientRect()
+
+      cursor.set({
+        position: { x: event.clientX - left, y: event.clientY - top },
+        data: { state: 'leave' }
+      })
+    })
+
+    // JIGSAW
     const jigsaw = new headbreaker.Canvas('puzzle', {
       width: 1000,
       height: 1000,
@@ -71,8 +208,8 @@ function startGame() {
     })
 
     jigsaw.autogenerate({
-      horizontalPiecesCount: 3,
-      verticalPiecesCount: 3
+      horizontalPiecesCount: 4,
+      verticalPiecesCount: 4
     })
 
     jigsaw.attachSolvedValidator()
@@ -85,22 +222,53 @@ function startGame() {
       }, 500)
     })
 
-    jigsaw.shuffle(0.8)
+    jigsaw.shuffle(0.6)
     jigsaw.draw()
+    setDragListenersOnPieces()
 
-    document.getElementById('persistent-import').addEventListener('click', function () {
+    jigsaw.onConnect((_piece, figure, _target, targetFigure) => {
+      // paint borders on click
+      // of conecting and conected figures
+      figure.shape.stroke('yellow')
+      targetFigure.shape.stroke('yellow')
+      jigsaw.redraw()
+
+      setTimeout(() => {
+        // restore border colors
+        // later
+        figure.shape.stroke('black')
+        targetFigure.shape.stroke('black')
+        jigsaw.redraw()
+        const gameState = jigsaw.puzzle.export({ compact: true })
+        updateGameChannel.publish('updateGameState', gameState)
+      }, 200)
+    })
+
+    updateGameChannel.subscribe('updateGameState', function (message) {
+      if (message.clientId === clientId) { return }
+      const jsonGameState = message.data
+
       jigsaw.clear()
-
-      const dump = JSON.parse(jsonExportArea.value)
-
-      jigsaw.renderPuzzle(headbreaker.Puzzle.import(dump))
+      jigsaw.renderPuzzle(headbreaker.Puzzle.import(jsonGameState))
       jigsaw.draw()
+      setDragListenersOnPieces()
     })
 
-    document.getElementById('persistent-export').addEventListener('click', function () {
-      const dump = jigsaw.puzzle.export({ compact: true })
+    function setDragListenersOnPieces () {
+      const figures = Object.values(jigsaw.figures)
+      const timesPerSecond1 = 14
+      let waitUntil1 = 0
 
-      jsonExportArea.value = JSON.stringify(dump, null, 2)
-    })
+      figures.forEach(it => {
+        it.group.on('dragmove', function () {
+          if (Date.now() >= waitUntil1) {
+            const gameState = jigsaw.puzzle.export({ compact: true })
+            updateGameChannel.publish('updateGameState', gameState)
+
+            waitUntil1 = Date.now() + 1000 / timesPerSecond1
+          }
+        })
+      })
+    }
   }
 }
